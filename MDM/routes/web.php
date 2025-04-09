dc4925f36ef1f844a002abad02ca34c6e3877abb
Lasith Amanda <lasithamanda1999@example.com>
2025-04-09 17:03:25 +0530

    Setup routes

--- MDM/routes/web.php
+++ MDM/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirecct()->route('dashboard');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    //Brands
+    Route::resource('brands', BrandController::class)->except(['show']);
+
+    //Categories
+    Route::resource('categories', CategoryController::class)->except(['show']);
+
+    //Items
+    Route::resource('items', ItemController::class)->except(['show']);
+
+});
+
+require __DIR__.'/auth.php';
